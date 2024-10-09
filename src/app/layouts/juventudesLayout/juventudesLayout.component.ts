20a74eaacb1b5f316dc229123bab82cd3159f5a8
knarfangelo <knarf2003angelo@gmail.com>
2024-10-09 12:41:12 -0500

    terminando

--- src/app/layouts/juventudesLayout/juventudesLayout.component.ts
+++ src/app/layouts/juventudesLayout/juventudesLayout.component.ts
@@ -21,9 +21,9 @@ import { FooterComponent } from "../footer/footer.component";
         <section class="contenido">
         En Acción Popular, los jóvenes son actores clave en la construcción del Perú que soñamos. Nuestras Juventudes promueven la participación activa de los jóvenes en la política y la vida cívica, impulsando el liderazgo y brindando oportunidades de formación para que puedan ser los futuros líderes del país. <br>
         <ul>
-          <li>Formación de Líderes: A través de programas y talleres, preparamos a la próxima generación de líderes que estarán a la vanguardia del cambio en el Perú.</li>
-          <li>Redes de Juventudes: Creamos espacios de diálogo y participación para los jóvenes de todas las regiones del país.</li>
-          <li>Acciones Sociales: Las Juventudes de Acción Popular organizan campañas solidarias y proyectos comunitarios que impactan positivamente en las comunidades más vulnerables.</li>
+          <li><strong>Formación de Líderes:</strong> A través de programas y talleres, preparamos a la próxima generación de líderes que estarán a la vanguardia del cambio en el Perú.</li>
+          <li><strong>Redes de Juventudes:</strong> Creamos espacios de diálogo y participación para los jóvenes de todas las regiones del país.</li>
+          <li><strong>Acciones Sociales:</strong> Las Juventudes de Acción Popular organizan campañas solidarias y proyectos comunitarios que impactan positivamente en las comunidades más vulnerables.</li>
         </ul>
         </section>
         <img src="juventudes/juventudes.png" alt="">
